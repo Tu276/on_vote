5bc434b6af4932cc69b1ca8c2b357f95837d4077
Tu276 <tu276nathan@gmail.com>
2023-04-05 17:22:16 +0300

    back up

--- onvote.py
+++ onvote.py
@@ -1,8 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
 @app.route('/')
-def hello():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html')
+
